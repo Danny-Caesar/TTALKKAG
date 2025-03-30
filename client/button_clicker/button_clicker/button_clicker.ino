1c7d9d0a6d973d64f367423d2f70fc5b29d0c79f
dbrud919 <dbrud919@naver.com>
2025-03-30 22:14:21 +0900

    feat: (초기각도설정, 원래대로 돌아오는 각도 설정)

--- client/button_clicker/button_clicker/button_clicker.ino
+++ client/button_clicker/button_clicker/button_clicker.ino
@@ -10,15 +10,26 @@
 #include <Servo.h>
 
 Servo myservo;  
-
+int initialAngle = 0;  // 서보모터의 초기 각도
+int returnAngle = 45;  // 서보모터가 돌아올 각도 (목표 각도)
 
 void setup() {
-  myservo.attach(3);  
+  // 서보모터를 3번 핀에 연결
+  myservo.attach(3);
+  
+  // 서보모터를 초기 각도로 설정
+  myservo.write(initialAngle);
+  
+  // 서보모터 초기화 후 1초 대기
+  delay(1000);  // 1초 대기
 }
 
-void loop() {   
-  myservo.write(150);                  
-  delay(15);         
-  myservo.write(30);                  
-  delay(15);                  
+void loop() {
+  // 서보모터를 목표 각도로 이동 (예: 90도)
+  myservo.write(returnAngle);
+  delay(1000);  // 1초 대기
+
+  // 서보모터를 초기 각도로 돌아가게 이동 (예: 0도)
+  myservo.write(initialAngle);
+  delay(1000);  // 1초 대기
 }
