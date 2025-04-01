d87d7f34efc123c3aec9e1066f298a870b7c5912
Danny-Caesar <donggunmaru@gmail.com>
2025-04-01 17:34:21 +0900

    feat: connack packet 구현

---
+++ broker/packet/connack_packet.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "mqtt_control_packet.h"
+#include <vector>
+
+class connack_packet : public mqtt_control_packet {
+public:
+    bool session_present = false;
+    uint8_t return_code = 0x00;
+
+    mqtt_packet_type type() const override { return mqtt_packet_type::CONNACK; }
+    void handle(socket_broker&) override { /* No need */ }
+
+    static std::unique_ptr<connack_packet> create(bool session_present, uint8_t return_code);
+
+    std::vector<uint8_t> serialize() const
+    {
+        std::vector<uint8_t> packet;
+
+        // Fixed Header
+        packet.push_back(0x20);  // CONNACK (Type = 2 << 4), flags = 0
+        packet.push_back(0x02);  // Remaining Length = 2
+
+        // Variable Header
+        uint8_t flags = session_present ? 0x01 : 0x00;
+        packet.push_back(flags);        // Connect Acknowledge Flags
+        packet.push_back(return_code);  // Connect Return Code
+
+        return packet;
+    }
+};
