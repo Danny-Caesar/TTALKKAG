4bcb4ecf827802558174dad255bda438ddecda0d
Danny-Caesar <donggunmaru@gmail.com>
2025-04-01 17:34:48 +0900

    feat: packet handler 추가가

--- broker/packet/mqtt_control_packet.cpp
+++ broker/packet/mqtt_control_packet.cpp
@@ -2,6 +2,9 @@
 #include <iostream>
 #include "mqtt_control_packet.h"
 #include "fixed_header.h"
+#include "variable_header.h"
+#include "connect_packet.h"
+#include "connack_packet.h"
 
 // Parse byte stream to a packet format.
 std::unique_ptr<mqtt_control_packet> mqtt_control_packet::mqtt_control_packet::parse(const uint8_t* data, size_t size)
@@ -15,140 +18,17 @@ std::unique_ptr<mqtt_control_packet> mqtt_control_packet::mqtt_control_packet::p
     // Debug fixed hedaer.
     header.print_fixed_header();
 
-    // Variable header pointer.
-    const uint8_t* p_variable_header = data + header.header_length;
+    // Payload pointer. (or variable header pointer to some packet types.)
+    const uint8_t* payload = data + header.header_length;
 
-    variable_header v_header;
     switch (header.packet_type)
     {
         case mqtt_packet_type::CONNECT:
-            v_header = variable_header::parse(p_variable_header, header.remaining_length);
-            v_header.print_variable_header();
-            return connect_packet::parse(p_variable_header, header.remaining_length);
+            return connect_packet::parse(payload, header.remaining_length);
+        case mqtt_packet_type::CONNACK:
+            return NULL;
         // Other parse functions...
         default:
             throw std::runtime_error("Unsupported packet type");
     }
-}
-
-// Parse byte stream to a fixed header format.
-fixed_header fixed_header::parse(const uint8_t* data, size_t size)
-{   
-    // Packet type and flags
-    uint8_t byte1 = data[0];
-    uint8_t packet_type_raw = (byte1 & 0xF0) >> 4;
-    uint8_t flags = byte1 & 0x0F;
-
-
-    uint32_t remaining_length = 0;
-    int multiplier = 1;
-    size_t index = 1;
-    
-    // Remaining length
-    while(true)
-    {
-        if(index >= size) throw std::runtime_error("Malformed remaining length. (Too short)");
-        
-        // Read integer value from data
-        uint8_t encoded_byte = data[index++];
-        remaining_length += (encoded_byte & 0x7F) * multiplier;
-
-        if(multiplier > 128 * 128 * 128) throw std::runtime_error("Malformed remaining length. (Too long)");
-
-        // Last byte
-        if ((encoded_byte & 0x80) == 0) break;
-
-        multiplier *= 128;
-    }
-
-    size_t header_length = index;
-    
-    return fixed_header
-    {
-        static_cast<mqtt_packet_type>(packet_type_raw),
-        flags,
-        remaining_length,
-        header_length
-    };
-}
-
-void fixed_header::print_fixed_header()
-{
-    std::cout<<"type: "             << (int) this->packet_type << '\n';
-    std::cout<<"flags: "            << (int) this->flags << '\n';
-    std::cout<<"remaining_length: " << this->remaining_length << '\n';
-    std::cout<<"header_length: "    << this->header_length << '\n';
-}
-
-variable_header variable_header::parse(const uint8_t* data, size_t size)
-{
-    size_t index = 0;
-
-    // 1. Protocol Name Length (2 bytes)
-    if (size < 2) throw std::runtime_error("Malformed variable header length. (Too short)");
-    uint16_t protocol_name_length = (data[index] << 8) | data[index + 1];
-    index += 2;
-    // v3.1.1 protocol name exception handle
-    
-
-    // 2. Protocol Name (n bytes)
-    if (size < index + protocol_name_length) throw std::runtime_error("Malformed variable header. (Wrong protocol name)");
-    std::string protocol_name = std::string(reinterpret_cast<const char*>(&data[index]), protocol_name_length);
-    index += protocol_name_length;
-
-    // 3. Protocol Level (1 byte)
-    if (size < index + 1) throw std::runtime_error("Malformed variable header. (Wrong protocol level)");
-    uint8_t protocol_level = data[index++];
-
-    // 4. Connect Flags (1 byte)
-    if (size < index + 1) throw std::runtime_error("Malformed variable header. (Wrong connect flags)");
-    uint8_t flags = data[index++];
-    bool username_flag   = (flags & 0b10000000) != 0;
-    bool password_flag   = (flags & 0b01000000) != 0;
-    bool will_retain     = (flags & 0b00100000) != 0;
-    uint8_t will_qos     = (flags & 0b00011000) >> 3;
-    bool will_flag       = (flags & 0b00000100) != 0;
-    bool clean_session   = (flags & 0b00000010) != 0;
-
-    // 5. Keep Alive (2 bytes)
-    if (size < index + 2) throw std::runtime_error("Malformed variable header. (Wrong keep alive)");
-    uint16_t keep_alive = (data[index] << 8) | data[index + 1];
-    index += 2;
-
-    return variable_header
-    {
-        protocol_name,
-        protocol_level,
-        username_flag,
-        password_flag,
-        will_retain,
-        will_qos,
-        will_flag,
-        clean_session,
-        keep_alive
-    };
-}
-
-void variable_header::print_variable_header()
-{
-    std::cout<<"protocol_name: "  << this->protocol_name<<'\n';
-    std::cout<<"protocol_level: " << (int)this->protocol_level<<'\n';
-    std::cout<<"username_flag: "  << this->connect_flags.username_flag<<'\n';
-    std::cout<<"password_flag: "  << this->connect_flags.password_flag<<'\n';
-    std::cout<<"will_retain: "    << this->connect_flags.will_retain<<'\n';
-    std::cout<<"will_qos: "       << (int)this->connect_flags.will_qos<<'\n';
-    std::cout<<"will_flag: "      << this->connect_flags.will_flag<<'\n';
-    std::cout<<"clean_session: "  << this->connect_flags.clean_session<<'\n';
-    std::cout<<"keep_alive: "     << this->keep_alive<<'\n';
-}
-
-std::unique_ptr<connect_packet> connect_packet::parse(const uint8_t* payload, size_t length)
-{
-    // Parse protocol name.
-
-    // Parse protocol version number.
-
-    // Parse connect flags.
-
-    // Parse keep alive timer.
 }
--- broker/packet/mqtt_control_packet.h
+++ broker/packet/mqtt_control_packet.h
@@ -1,47 +1,17 @@
 #pragma once
 
 #include <memory>
-#include <string>
 #include "mqtt_packet_types.h"
 
-// MQTT base packet
-class mqtt_control_packet
-{
-    public:
-        virtual ~mqtt_control_packet() = default;
-        // Control packet type
-        virtual mqtt_packet_type type() const = 0;
+class socket_broker; // forward declaration
 
-        // Parse byte stream to a packet format.
-        static std::unique_ptr<mqtt_control_packet> parse(const uint8_t* data, size_t size);
-};
+class mqtt_control_packet {
+public:
+    virtual ~mqtt_control_packet() = default;
+    virtual mqtt_packet_type type() const = 0;
 
-class connect_packet : public mqtt_control_packet
-{
-    public:
-        std::string client_id;
-        bool clean_session;
-    
-        static std::unique_ptr<connect_packet> parse(const uint8_t* payload, size_t length);
-    
-        mqtt_packet_type type() const override { return mqtt_packet_type::CONNECT; }
-};
+    // Packet handler
+    virtual void handle(socket_broker& broker) = 0;
 
-struct variable_header
-{
-    std::string protocol_name;
-    uint8_t protocol_level;
-    struct 
-    {
-        bool username_flag;
-        bool password_flag;
-        bool will_retain;
-        uint8_t will_qos;
-        bool will_flag;
-        bool clean_session;
-    } connect_flags;
-    uint16_t keep_alive;
-
-    static variable_header parse(const uint8_t* data, size_t size);
-    void print_variable_header();
+    static std::unique_ptr<mqtt_control_packet> parse(const uint8_t* data, size_t size);
 };
