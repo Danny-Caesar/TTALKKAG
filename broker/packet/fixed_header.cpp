bee2c19b3913fc2d0caf2fac6b680354d2d24aa7
Danny-Caesar <donggunmaru@gmail.com>
2025-04-06 17:30:02 +0900

    feat: remaining length 관련 기능 함수화
    remaining length의 인코딩과 디코드 기능을 함수로 분리

--- broker/packet/fixed_header.cpp
+++ broker/packet/fixed_header.cpp
@@ -9,21 +9,37 @@ fixed_header fixed_header::parse(const uint8_t* data, size_t size)
     uint8_t packet_type_raw = (byte1 & 0xF0) >> 4;
     uint8_t flags = byte1 & 0x0F;
 
+    size_t index = 1;
 
     // 2. Remaining length
+    std::pair<uint32_t, size_t> remaining_length = fixed_header::decode_remaining_length(&data[index], size - index);
+
+    size_t header_length = index + remaining_length.second;
+
+    return fixed_header
+    {
+        static_cast<mqtt_packet_type>(packet_type_raw),
+        flags,
+        remaining_length.first,
+        header_length
+    };
+}
+
+std::pair<uint32_t, size_t> fixed_header::decode_remaining_length(const uint8_t* data, size_t size)
+{
     uint32_t remaining_length = 0;
     int multiplier = 1;
-    size_t index = 1;
-    
+    size_t index = 0;
+
     while(true)
-    {
-        if(index >= size) throw std::runtime_error("Malformed remaining length. (Too short)");
-        
+    {   
+        if(size < index + 1) throw std::runtime_error("Malformed remaining length: Too short.");
+
         // Read integer value from data
         uint8_t encoded_byte = data[index++];
         remaining_length += (encoded_byte & 0x7F) * multiplier;
 
-        if(multiplier > 128 * 128 * 128) throw std::runtime_error("Malformed remaining length. (Too long)");
+        if(multiplier > 128 * 128 * 128) throw std::runtime_error("Malformed remaining length: Too long.");
 
         // Last byte
         if ((encoded_byte & 0x80) == 0) break;
@@ -31,26 +47,32 @@ fixed_header fixed_header::parse(const uint8_t* data, size_t size)
         multiplier *= 128;
     }
 
-    size_t header_length = index;
-    
-    fixed_header f_header
-    {
-        static_cast<mqtt_packet_type>(packet_type_raw),
-        flags,
-        remaining_length,
-        header_length
-    };
+    return std::pair<uint32_t, size_t>(remaining_length, index);
+}
 
-    f_header.debug();
+std::vector<uint8_t> fixed_header::encode_remaining_length(uint32_t remaining_length)
+{
+    std::vector<uint8_t> buf;
 
-    return f_header;
+    do {
+        uint8_t encoded_byte = remaining_length % 128;
+        remaining_length /= 128;
+        if (remaining_length > 0)
+            encoded_byte |= 0x80;
+        buf.push_back(encoded_byte);
+    } while (remaining_length > 0);
+
+    return buf;
 }
 
 void fixed_header::debug()
 {
-    std::cout << "----Fixed Header----\n";
-    std::cout << "type: "             << (int) this->packet_type << '\n';
-    std::cout << "flags: "            << (int) this->flags       << '\n';
+    std::cout << "---Fixed Header---\n";
+    std::cout << "type: "             << (int)this->packet_type  << '\n';
+    std::cout << "flags: [ ";
+    for(int i = 3; i >= 0; i--)
+        std::cout << (int)((this->flags >> i) & 0x01) << ' ';
+    std::cout << "] \n";
     std::cout << "remaining_length: " << this->remaining_length  << '\n';
     std::cout << "header_length: "    << this->header_length     << "\n\n";
 }
