86c0b62ee57ba32507b6a1896de5d3993594e8a7
Danny-Caesar <donggunmaru@gmail.com>
2025-04-12 23:03:23 +0900

    feat: session manager 개선
    session을 unique_ptr로 관리하도록 변경

--- broker/session/session_manager.cpp
+++ broker/session/session_manager.cpp
@@ -3,7 +3,7 @@
 #include "session_manager.h"
 
 // Container initailize.
-std::unordered_map<std::string, mqtt_session*> session_manager::_session_map;
+std::unordered_map<std::string, std::unique_ptr<mqtt_session>> session_manager::_session_map;
 
 session_manager& session_manager::get_instance()
 {
@@ -11,35 +11,73 @@ session_manager& session_manager::get_instance()
     return instance;
 }
 
-void session_manager::register_session(const std::string& client_id, mqtt_session* session)
+void session_manager::register_session(const std::string& client_id, std::unique_ptr<mqtt_session> session)
 {
-    // Duplicated client check.
-    if(_session_map.find(client_id) != _session_map.end()) return;
+    auto it = _session_map.find(client_id);
+
+    // Update clean session.
+    if(it != _session_map.end())
+        _session_map[client_id]->clean_session = session->clean_session;
+
+    // Check whether value differs.
+    // later...
 
     // Register client.
-    _session_map[client_id] = session;
+    _session_map[client_id] = std::move(session);
 }
 
 void session_manager::remove_session(const std::string& client_id)
 {
-    // _session_map[client_id]->socket->close();
+    _session_map[client_id]->socket->close();
     _session_map.erase(client_id);
 }
 
-mqtt_session* session_manager::get_session(const std::string& client_id)
+void session_manager::open_session(const std::string& client_id, socket_broker* socket)
 {
-    if(_session_map.find(client_id) != _session_map.end())
-        return NULL;
-    
-    return _session_map[client_id];
+    _session_map[client_id]->open_session(socket);
+}
+
+void session_manager::close_session(const std::string& client_id)
+{
+    _session_map[client_id]->close_session();
+}
+
+bool session_manager::has_session(const std::string& client_id)
+{
+    auto session = _session_map.find(client_id);
+
+    if(session != _session_map.end())
+        return true;
+    else
+        return false;
+}
+
+bool session_manager::is_clean_session(const std::string& client_id)
+{
+    return _session_map[client_id]->clean_session;
 }
 
 void session_manager::debug()
 {
-    std::cout << "----Client list----\n";
-    for(auto c : _session_map)
+    std::cout << "----Session list----\n";
+    for(auto it = _session_map.begin(); it != _session_map.end(); it++)
     {
-        std::cout << c.first << '\n';
+        std::cout << it->first << '\n';
     }
-    std::cout << '\n';
+    std::cout<<'\n';
 }
+
+//
+// legacy
+//
+
+// mqtt_session* session_manager::find_session(const std::string& client_id)
+// {
+//     auto it = _session_map.find(client_id);
+//     if(it == _session_map.end())
+//         return NULL;
+
+//     it->second->debug();
+
+//     return it->second;
+// }
--- broker/session/session_manager.h
+++ broker/session/session_manager.h
@@ -7,17 +7,20 @@ class session_manager
 {
 public:
     static session_manager& get_instance();
-    void register_session(const std::string& client_id, mqtt_session* session);
+    void register_session(const std::string& client_id, std::unique_ptr<mqtt_session> session);
     void remove_session(const std::string& client_id);
-    void open_session(socket_broker* socket);
-    void close_session();
-    mqtt_session* get_session(const std::string& client_id);
+    void open_session(const std::string& client_id, socket_broker* socket);
+    void close_session(const std::string& client_id);
+    bool has_session(const std::string& client_id);
+    bool is_clean_session(const std::string& client_id);
     void debug();
+
+    // mqtt_session* find_session(const std::string& client_id);
 
 private:
     session_manager() = default;
     session_manager(const session_manager&) = delete;
     session_manager operator=(const session_manager&) = delete;
     
-    static std::unordered_map<std::string, mqtt_session*> _session_map;
+    static std::unordered_map<std::string, std::unique_ptr<mqtt_session>> _session_map;
 };
