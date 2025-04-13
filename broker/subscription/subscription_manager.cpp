4d7d94e85b43fa98ccd6af88e6c1e5d482833d48
Danny-Caesar <donggunmaru@gmail.com>
2025-04-13 16:59:41 +0900

    feat: subscription 디버깅
    구독 전체/토픽별 디버깅 함수 추가

--- broker/subscription/subscription_manager.cpp
+++ broker/subscription/subscription_manager.cpp
@@ -1,8 +1,9 @@
-#include "subscription_manager.h"
+#include <iostream>
 #include <algorithm>
+#include "subscription_manager.h"
 
 // Initalization
-std::unordered_map<std::string, std::vector<subscription>> subscription_manager::mqtt_subscriptions;
+std::unordered_map<std::string, std::vector<subscription>> subscription_manager::_subscription_map;
 
 // Return singleton instance.
 subscription_manager& subscription_manager::get_instance()
@@ -16,7 +17,7 @@ subscription_manager& subscription_manager::get_instance()
 void subscription_manager::add_subscription(const std::string& topic, const std::string& client_id, uint8_t qos)
 {
     // Get reference of the list of subscriptions for the topic
-    auto& subs = mqtt_subscriptions[topic];
+    auto& subs = _subscription_map[topic];
 
     auto it = std::find_if(subs.begin(), subs.end(), [&](const subscription& s)
     {
@@ -40,8 +41,8 @@ void subscription_manager::add_subscription(const std::string& topic, const std:
 void subscription_manager::remove_subscription(const std::string& topic, const std::string& client_id)
 {
     // Check whether the topic exists in the subscription map.
-    auto it = mqtt_subscriptions.find(topic);
-    if (it == mqtt_subscriptions.end()) return; // No such topic.
+    auto it = _subscription_map.find(topic);
+    if (it == _subscription_map.end()) return; // No such topic.
 
     // Get reference to the list of subscriptions for this topic.
     auto& subs = it->second;
@@ -57,7 +58,7 @@ void subscription_manager::remove_subscription(const std::string& topic, const s
     // If the subscription list is now empty, remove the topic entry.
     if (subs.empty())
     {
-        mqtt_subscriptions.erase(it);
+        _subscription_map.erase(it);
     }
 }
 
@@ -65,22 +66,69 @@ void subscription_manager::remove_subscription(const std::string& topic, const s
 std::vector<subscription> subscription_manager::get_subscribers(const std::string& topic)
 {
     // Check if the topic exists in the subscription map
-    auto it = mqtt_subscriptions.find(topic);
-    if (it == mqtt_subscriptions.end())
+    auto it = _subscription_map.find(topic);
+    if (it == _subscription_map.end())
         return std::vector<subscription>();
 
     return it->second;
 }
 
-// Return specific subscription.
-subscription subscription_manager::get_subscribtion(const std::string& topic, const std::string& client_id)
+void subscription_manager::debug()
 {
-    std::vector<subscription> sub = mqtt_subscriptions[topic];
-    
-    auto it = std::find_if(sub.begin(), sub.end(), [&](const subscription s)
+    std::cout << "----Subscription list----\n";
+    for(auto it_topic = _subscription_map.begin(); it_topic != _subscription_map.end(); it_topic++)
     {
-        return s.client_id == client_id;
-    });
-
-    return *it;
+        std::cout << "---" << it_topic->first << "---\n";
+        for(auto it_client = it_topic->second.begin(); it_client != it_topic->second.end(); it_client++)
+        {
+            std::cout << it_client->client_id << '(' << (int)it_client->qos << ")\n";
+        }
+        std::cout << '\n';
+    }
+    std::cout << '\n';
 }
+
+void subscription_manager::debug(const std::string& topic)
+{
+    std::cout << "----Subscription list----\n";
+    auto it_topic = _subscription_map.find(topic);
+
+    std::cout << "---" << it_topic->first << "---\n";
+
+    for(auto it_client = it_topic->second.begin(); it_client != it_topic->second.end(); it_client++)
+    {
+        std::cout << it_client->client_id << '(' << (int)it_client->qos << ")\n";
+    }
+    std::cout << '\n';
+}
+
+// void subscription_manager::debug(const std::string& client_id)
+// {
+//     std::cout << "----Subscription list----\n";
+//     std::cout << "---" << client_id << "---\n";
+//     for(auto it_topic = _subscription_map.begin(); it_topic != _subscription_map.end(); it_topic++)
+//     {
+//         for(auto it_client = it_topic->second.begin(); it_client != it_topic->second.end(); it_client++)
+//         {
+//             if(it_client->client_id == client_id)
+//             {   
+//                 std::cout << it_topic->first << '(' << (int)it_client->qos << ")\n";
+//                 break;
+//             }
+//         }
+//     }
+//     std::cout << '\n';
+// }
+
+// // Return specific subscription.
+// subscription subscription_manager::get_subscribtion(const std::string& topic, const std::string& client_id)
+// {
+//     std::vector<subscription> sub = _subscription_map[topic];
+    
+//     auto it = std::find_if(sub.begin(), sub.end(), [&](const subscription s)
+//     {
+//         return s.client_id == client_id;
+//     });
+
+//     return *it;
+// }
--- broker/subscription/subscription_manager.h
+++ broker/subscription/subscription_manager.h
@@ -9,11 +9,15 @@ public:
     void add_subscription(const std::string& topic, const std::string& client_id, uint8_t qos);
     void remove_subscription(const std::string& topic, const std::string& client_id);
     std::vector<subscription> get_subscribers(const std::string& topic);
-    subscription get_subscribtion(const std::string& topic, const std::string& client_id);
+
+    void debug();
+    void debug(const std::string& topic);
+    // void debug(const std::string& client_id);
+    // subscription get_subscribtion(const std::string& topic, const std::string& client_id);
 
 private:
     subscription_manager() = default;
-    static std::unordered_map<std::string, std::vector<subscription>> mqtt_subscriptions;
+    static std::unordered_map<std::string, std::vector<subscription>> _subscription_map;
 
     subscription_manager(const subscription_manager&) = delete;
     subscription_manager operator=(const subscription_manager&) = delete;
