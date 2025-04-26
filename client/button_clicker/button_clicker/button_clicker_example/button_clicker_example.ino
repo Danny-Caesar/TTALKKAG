26a25115bbcd01a1159a89f90b9e3fc1175d26c5
김동건 <donggunmaru@gmail.com>
2025-04-26 23:09:59 +0900

    feat: 연결 메시지 발행
    서버에 부팅(등록)을 알리는 메시지 발행

--- client/button_clicker/button_clicker/button_clicker_example/button_clicker_example.ino
+++ client/button_clicker/button_clicker/button_clicker_example/button_clicker_example.ino
@@ -1,8 +1,15 @@
 #include <WiFi.h>
 #include <PubSubClient.h>
 #include "ESP32Servo.h"
+#include <ArduinoJson.h>
 
 #define SERVO_PIN 5
+
+// 디바이스 정보
+const char* client_id = "bc0x00";
+const char* device_type = "button";
+char* device_name = "button_clicker";
+char json_payload_connect[256];
 
 // WiFi 정보
 const char* ssid = "kdg";
@@ -11,9 +18,11 @@ const char* password = "12345678";
 // MQTT 정보
 const char* mqtt_server = "192.168.137.39"; // MQTT Broker IPv4
 const int mqtt_port = 1883;
-const char* mqtt_user = "";
-const char* mqtt_password = "";
-const char* topic_click = "server/click";
+const char* mqtt_user = NULL;
+const char* mqtt_password = NULL;
+const char* topic_connect = "hub/connect";
+String topic_disconnect;
+String topic_click;
 
 // Servo 정보
 const int angle_initial = 0;
@@ -48,6 +57,34 @@ void setup_wifi() {
   Serial.println(WiFi.localIP()); // 본 디바이스의 IPv4
 }
 
+void reconnect() {
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    
+    // Client id 할당
+    String clientId = "button_clicker_";
+    clientId += String(random(0xffff), HEX);
+
+    // 서버에 MQTT 연결
+    if (client.connect(clientId.c_str(), mqtt_user, mqtt_password)) {
+      // 성공
+      Serial.println("connected");
+      
+      // 연결 토픽 발행 (retain = false, QoS: 1)
+      client.publish(topic_connect, (const uint8_t*)json_payload_connect, strlen(json_payload_connect), false);
+
+      // 필요한 토픽 구독
+      client.subscribe(topic_click.c_str(), 1);
+    } else {
+      // 실패
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" retrying in a second...");
+      delay(1000);
+    }
+  }
+}
+
 // 메시지 수신 시 호출되는 함수
 void callback(char* topic, byte* message, unsigned int length) {
   // 수신한 토픽 확인
@@ -63,34 +100,30 @@ void callback(char* topic, byte* message, unsigned int length) {
   Serial.println(payload);
 
   // Click 토픽 처리
-  if (strcmp(topic, topic_click) == 0) {
+  if (strcmp(topic, topic_click.c_str()) == 0) {
     click();
   }
 }
 
-void reconnect() {
-  while (!client.connected()) {
-    Serial.print("Attempting MQTT connection...");
-    
-    // Client id 할당
-    String clientId = "button_clicker_";
-    clientId += String(random(0xffff), HEX);
+// 연결 메시지 초기화
+void setup_payload_connect() {
+  // 연결 메시지 생성
+  StaticJsonDocument<200> doc;
+  doc["clientId"] = client_id;
+  doc["type"] = device_type;
+  doc["name"] = device_name;
+  
+  // Json 컨테이너를 문자열로 변환
+  serializeJson(doc, json_payload_connect);
+}
 
-    // 서버에 MQTT 연결
-    if (client.connect(clientId.c_str(), mqtt_user, mqtt_password)) {
-      // 성공
-      Serial.println("connected");
-      
-      // 필요한 토픽 구독
-      client.subscribe(topic_click, 1);
-    } else {
-      // 실패
-      Serial.print("failed, rc=");
-      Serial.print(client.state());
-      Serial.println(" retrying in a second...");
-      delay(1000);
-    }
-  }
+// 토픽 초기화
+void setup_topic() {
+  // disconnect 토픽 설정
+  topic_disconnect = String("server/disconnect/") + device_type + "/" + client_id;
+
+  // click 토픽 설정
+  topic_click = String("server/") + device_type + "/" + client_id + "/" + "action";
 }
 
 // 서보 모터 초기화
@@ -110,8 +143,15 @@ void click() {
 
 void setup() {
   Serial.begin(115200);
-  setup_servo();
+
+
+  setup_payload_connect();
+  setup_topic();
+
   setup_wifi();
+
+  setup_servo();
+
   client.setServer(mqtt_server, mqtt_port);
   client.setCallback(callback);
 }
